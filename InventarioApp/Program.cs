5087dd3d4e6cbd0e43d62eae2e7bcf45625153b7
Sanchez Malvafon Juan Gerardo <sisjsanchez@porunpaismejor.com.mx>
2026-05-19 10:26:57 -0600

    Primer push con estructura base de proyecto y configuración de gitignore

---
+++ InventarioApp/Program.cs
@@ -0,0 +1,26 @@
+﻿// ============================================================
+// SISTEMA DE INVENTARIO - Clase 1.1
+// Estado: Mensaje de bienvenida
+// ============================================================
+
+using System.Reflection;
+using System.Runtime.Intrinsics.Arm;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"Versión: 1.0.0");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine($"Version:{version}");
+Console.WriteLine();
+Console.WriteLine("Estructura del proyecto:");
+Console.WriteLine("Configuración .csproj");
+Console.WriteLine("Carpeta scr/ creada");
+Console.WriteLine("Metadatos configurados");
+Console.WriteLine();
+Console.WriteLine("Próximo paso: Agregar argumentos CLI y configuraciones de repositorio en Github");
