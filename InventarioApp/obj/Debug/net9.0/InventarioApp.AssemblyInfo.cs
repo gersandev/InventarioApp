0038fdb631d4659288416edf81a149ee6c3d04bc
Sanchez Malvafon Juan Gerardo <sisjsanchez@porunpaismejor.com.mx>
2026-05-19 16:05:55 -0600

    Nuevo archivo README.md

--- InventarioApp/obj/Debug/net9.0/InventarioApp.AssemblyInfo.cs
+++ InventarioApp/obj/Debug/net9.0/InventarioApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Sistema de gestion de inventario - Curso Fundamentos .NET")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5087dd3d4e6cbd0e43d62eae2e7bcf45625153b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventarioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventarioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
